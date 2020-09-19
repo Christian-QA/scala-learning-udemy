f567cfb55f4f835de84390039ea66cfb761383ee
Chrisctr <credfern96@gmail.com>
2020-09-19 15:50:40 +0100

    Added exercise on OOP

--- src/exercises/MyListCo.scala
+++ src/exercises/MyListCo.scala
@@ -16,6 +16,13 @@ package exercises
     def add[B >: A](element: B): MyListCo[B]
     def printElements: String
     override def toString: String = "["+ printElements +"]"
+
+    def map[B](transformer: MyTransformer[A,B]): MyListCo[B]
+    def flatMap[B](transformer: MyTransformer[A,MyListCo[B]]): MyListCo[B]
+    def filter(predicate: MyPredicate[A]): MyListCo[A]
+
+    // Concatenation
+    def ++[B >: A](list: MyListCo[B]): MyListCo[B]
   }
 
   object Empty extends MyListCo[Nothing] {
@@ -24,6 +31,12 @@ package exercises
     def isEmpty: Boolean = true
     def add[B >: Nothing](element: B): MyListCo[B] = new Cons(element, Empty)
     override def printElements: String = ""
+
+    def map[B](transformer: MyTransformer[Nothing,B]): MyListCo[B] = Empty
+    def flatMap[B](transformer: MyTransformer[Nothing,MyListCo[B]]): MyListCo[B] = Empty
+    def filter(predicate: MyPredicate[Nothing]): MyListCo[Nothing] = Empty
+
+    def ++[B >: Nothing](list: MyListCo[B]): MyListCo[B] = list
   }
 
   class Cons[+A](h: A, t: MyListCo[A]) extends MyListCo[A] {
@@ -35,7 +48,52 @@ package exercises
     def printElements: String =
       if (t.isEmpty) "" + h
       else h + " " + t.printElements
+
+    /*
+      [1,2,3].filter(n % 2 == 0) =
+      [2, 3].filter(n % 2 == 0) =
+      = new Cons(2, [3].filter(n % 2 == 0))
+      = new Cons(2, Empty.filter(n % 2 == 0))
+      = new Cons(2, Empty)
+     */
+
+    def filter(predicate: MyPredicate[A]): MyListCo[A] =
+      if (predicate.test(h)) new Cons(h, t.filter(predicate))
+      else t.filter(predicate)
+
+    /*
+      [1,2,3].map(n * 2)
+      = new Cons(2, [2,3].map(2 * 2))
+      = new Cons(2, new Cons(4, [3].map(n * 2)))
+      = new Cons(2, new Cons(4, new Cons(6, Empty.map(n * 2))))
+      = new Cons(2, new Cons(4, new Cons(6, Empty))))
+     */
+
+    def map[B](transformer: MyTransformer[A,B]): MyListCo[B] =
+      new Cons(transformer.transform(h), t.map(transformer))
+
+    /*
+      [1,2] ++ [3,4,5]
+      = new Cons(1, [2] ++ [3,4,5])
+      = new Cons(1, new Cons(2, Empty ++ [3, 4, 5]))
+      = new Cons(1, new Cons(2, [3,4,5]))
+      = new Cons(1, new Cons(2, new Cons(3, new Cons(4, new Cons(5)))))
+     */
+
+    def ++[B >: A](list: MyListCo[B]): MyListCo[B] = new Cons(h, t ++ list)
+ 
+    def flatMap[B](transformer: MyTransformer[A, MyListCo[B]]): MyListCo[B] =
+      transformer.transform(h) ++ t.flatMap(transformer)
   }
+
+  trait MyPredicate[-T] {
+    def test(element: T): Boolean
+  }
+
+  trait MyTransformer[-A, B] {
+    def transform(element: A): B
+  }
+
   object ListTest extends App {
     val listOfIntegers: MyListCo[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
     val listOfStrings: MyListCo[String] = new Cons("Hello", new Cons("My", new Cons("Boy", Empty)))
--- src/lectures/part2oop/AnonymousClasses.scala
+++ src/lectures/part2oop/AnonymousClasses.scala
@@ -1,8 +1,50 @@
 package lectures.part2oop
 
-object AnonymousClasses {
+object AnonymousClasses extends App{
 
   //Udemy: Lecture 14 - Anonymous Classes
+  abstract class Animal {
+    def eat: Unit
+  }
 
-  
+
+
+  // Anonymous Class
+  val funnyAnimal: Animal = new Animal {
+    override def eat: Unit = println("AHAWHAHAHAHAHAHA")
+  }
+  /*
+    Equivalent with
+
+    class AnonymousClasses$$anon$1 extends Animal {
+      override def eat: Unit = println("AHAWHAHAHAHAHAHA")
+    }
+   */
+
+  println(funnyAnimal.getClass)
+
+  class Person(name: String) {
+    def sayHit: Unit = println(s"Hi, my name is $name, how can I help?")
+  }
+
+  val jim = new Person("Jim") {
+    override def sayHit: Unit = println(s"Hi, my name is Jim, how can I be of service")
+  }
+
+  /*
+  1. Generic trait MyPredicate[-T] with a little method test(t) => Boolean
+  2. Generic trait MyTransformer[-A, B] with a method transform(A) => B
+  3. MyList:
+    - map(transformer) => MyList
+    - filter(predicate) => MyList
+    - flatMap(transformer from A to MyList[B]) => MyList[B]
+
+    class EvenPredicate extends MyPredicate[Int]
+    class StringToIntTransformer extends MyTransformer[String, Int]
+
+    [1, 2, 3].map(n * 2) = [2, 4, 6]
+    [1, 2, 3, 4].filter(n % 2) = [2, 4]
+    [1, 2, 3].flatmap(n => [n, n+1] => [1, 2, 2, 3, 3, 4]
+  */
+
 }
