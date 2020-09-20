90c697fad882fe98e164e8275a4458ebd330c516
Chrisctr <credfern96@gmail.com>
2020-09-20 18:14:03 +0100

    Added lecture notes on sequences

--- src/lectures/part3fp/MapFlatmapFilterFor.scala
+++ src/lectures/part3fp/MapFlatmapFilterFor.scala
@@ -2,6 +2,66 @@ package lectures.part3fp
 
 object MapFlatmapFilterFor extends App {
 
-  
+  val list = List(1,2,3)
+  println(list.head)
+
+  // map
+  println(list.map(_ + 1))
+  println(list.map(_ + " is a number"))
+
+  // filter
+  println(list.filter(_ % 2 == 0))
+
+  // flatMap
+  val toPair = (x: Int) => List(x, x+1)
+  println(list.flatMap(toPair))
+
+  /// Exercise
+  // print all combinations between two lists
+  val numbers = List(1,2,3,4)
+  val chars = List('a','b','c','d')
+  val colours = List("black", "white")
+  // List("a1", "a2"... "d4")
+
+  // "iterating"
+  val combinations = numbers.flatMap(n => chars.flatMap(c => colours.map(colour => "" + c + n + colour)))
+  println(combinations)
+
+  // foreach
+  list.foreach(println)
+
+  // for-comprehensions
+  val forCombinations = for {
+    n <- numbers
+    c <- chars
+    colour <- colours
+  } yield "" + c + n + "" + colour
+  println(forCombinations)
+  // This is identical to the flatMap iteration style used above under "iterating", only far more human readable
+
+  val forCombinations2 = for {
+    n <- numbers if n % 2 == 0
+    c <- chars
+    colour <- colours
+  } yield "" + c + n + "" + colour
+  println(forCombinations2)
+  // Identical to: val combinations = numbers.filter(_ % 2 == 0).numbers.flatMap(n => chars.flatMap(c => colours.map(colour => "" + c + n + colour)))
+
+  for {
+    n <- numbers
+  } println(n)
+
+  // Syntax Overload
+  list.map { x =>
+    x * 2
+  }
+
+  /*
+    Exercise
+    A small collection of at most ONE element - Maybe[+T]
+      - map, flatMap, filter
+
+      Can be found in the exercises package as an abstract class called Maybe
+   */
 
 }
---
+++ src/lectures/part3fp/Sequences.scala
@@ -0,0 +1,77 @@
+package lectures.part3fp
+
+import scala.util.Random
+
+object Sequences extends App {
+
+  /// Sequences
+  // A very general interface for datastructures that have a well defined order and can be indexed
+  val aSequence = Seq(1,5,3,4)
+  println(aSequence)
+  println(aSequence.reverse)
+  println(aSequence(2))
+  println(aSequence ++ Seq(11,6,7))
+  println(aSequence.sorted)
+
+  /// Ranges
+  val aRange: Seq[Int] = 1 to 10 // until to exclude 10
+  aRange.foreach(println)
+
+  (1 to 10).foreach(x => println("Hello"))
+
+  /// Lists
+  val aList = List(1,2,3)
+  val prepended = 42 +: aList :+ 89
+  println(prepended)
+
+  val apples5 = List.fill(5)("apple")
+  println(apples5)
+  println(aList.mkString("-|-"))
+
+  // Arrays
+  val numbers = Array(1,2,3,4)
+  val treeElements = Array.ofDim[Int](3)
+  println(treeElements) // Returns hashcode
+  treeElements.foreach(println) // Returns three 0s
+
+  // Mutation
+  numbers(2) = 0 // Syntactic sugar for numbers.update(2, 0)
+  println(numbers.mkString(" "))
+
+  // Arrays and Seq
+  val numbersSeq: Seq[Int] = numbers // Implicit Conversion
+  println(numbersSeq)
+
+  /// Vectors
+  // The default implementation for immutable sequences
+  val vector: Vector[Int] = Vector(1,2,3)
+  println(vector)
+
+  // Vectors vs Lists
+  val maxRuns = 1000
+  val maxCapacity = 1000000
+  def getWriteTime(collection: Seq[Int]): Double = {
+    val r = new Random
+    val times = for {
+      it <- 1 to maxRuns
+    } yield {
+      val currentTime = System.nanoTime()
+      collection.updated(r.nextInt(maxCapacity), r.nextInt())
+      System.nanoTime() - currentTime
+    }
+    times.sum * 1.0 / maxRuns
+  }
+
+  val numbersList = (1 to maxCapacity).toList
+  val numbersVector = (1 to maxCapacity).toVector
+
+  // Keeps reference to tail
+  // Updating an element in the middle takes a long time
+  // Operation takes 1.19724549E7 nanoseconds (11 milliseconds)
+  println(getWriteTime(numbersList))
+
+  // Depth of the tree is small
+  // Needs to replace an entire 32-element chunk
+  // Operation takes 5016.4 nanoseconds (5 microseconds)
+  println(getWriteTime(numbersVector))
+}
